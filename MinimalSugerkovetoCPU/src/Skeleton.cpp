eaf5592044c7799bcb4151d8a857d282798f65f2
Babos Dávid <40315291+FokaKefir@users.noreply.github.com>
2023-10-28 23:15:56 +0200

    add hazi2

--- MinimalSugerkovetoCPU/src/Skeleton.cpp
+++ MinimalSugerkovetoCPU/src/Skeleton.cpp
@@ -18,7 +18,10 @@ struct Hit {
 
 struct Ray {
     vec3 start, dir;
-    Ray(vec3 _start, vec3 _dir) { start = _start; dir = normalize(_dir); }
+    Ray(vec3 _start, vec3 _dir) {
+        start = _start;
+        dir = normalize(_dir);
+    }
 };
 
 class Intersectable {
@@ -105,7 +108,7 @@ public:
 
         vec3 kd(0.3f, 0.2f, 0.1f), ks(2, 2, 2);
         Material * material = new Material(kd, ks, 50);
-        for (int i = 0; i < 500; i++)
+        for (int i = 0; i < 100; i++)
             objects.push_back(new Sphere(vec3(rnd() - 0.5f, rnd() - 0.5f, rnd() - 0.5f), rnd() * 0.1f, material));
     }
 
